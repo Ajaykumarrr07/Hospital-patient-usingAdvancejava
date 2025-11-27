f888e400e47ada08a9b8ba04424ebf29ed8a9f4a
indurmaniteja444 <indurmaniteja44@gmail.com>
2025-11-27 16:32:27 +0530

    save and delete operation

---
+++ src/main/java/com/alpha/Hospital/Controller/DoctorController.java
@@ -0,0 +1,33 @@
+package com.alpha.Hospital.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alpha.Hospital.Entity.Patient;
+import com.alpha.Hospital.Service.PatientService;
+
+@RestController
+public class DoctorController {
+	@Autowired
+	private PatientService ps;
+	
+	@PostMapping("/savepatient")
+	public void savePatient(@RequestBody Patient s) {
+		ps.savePatient(s);
+		
+	}
+	
+	
+	@DeleteMapping("delete")
+	public void deletePatient(@RequestParam int id)
+	{
+		ps.deletePatient(id);
+	}
+	
+	
+
+}
